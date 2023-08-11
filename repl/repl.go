6650c0294fc6dd0d0ccfdfdab44b60d702e26988
Arjun Malhotra <arjun.malhotra@openly.com>
2023-08-11 10:38:36 -0500

    Adding main & REPL.

---
+++ repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/arjunmalhotra1/monkey-interpreter/lexer"
+	"github.com/arjunmalhotra1/monkey-interpreter/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
