b6ab808fb5a0c521de710fc02eb04716c188dc89
Arjun Malhotra <arjun.malhotra@openly.com>
2023-08-19 15:41:23 -0500

    Write a TestString function.

---
+++ ast/ast_test.go
@@ -0,0 +1,38 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/arjunmalhotra1/monkey-interpreter/token"
+)
+
+// let myVar = anotherVar
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "myVar",
+					},
+					Value: "myVar",
+				},
+				Value: &Identifier{ // Value is an expression and Identifier implements the expression interface.
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "anotherVar",
+					},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got = %q", program.String())
+	}
+}
