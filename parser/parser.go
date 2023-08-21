b5d6b4b4ec811532125ed121d2eb11d1b7aa7b77
Arjun Malhotra <arjun.malhotra@openly.com>
2023-08-21 01:00:53 -0500

    1. Add Integer Literal & 2. Add parseIntegerLiteral() & register it in registerPrefix.

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/arjunmalhotra1/monkey-interpreter/ast"
 	"github.com/arjunmalhotra1/monkey-interpreter/token"
@@ -42,17 +43,12 @@ func New(l *lexer.Lexer) *Parser {
 	}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 	return p
-}
-
-func (p *Parser) parseIdentifier() ast.Expression {
-	return &ast.Identifier{
-		Token: p.curToken, Value: p.curToken.Literal,
-	}
 }
 
 func (p *Parser) Errors() []string {
@@ -169,4 +165,28 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		return false
 	}
 
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{
+		Token: p.curToken, Value: p.curToken.Literal,
+	}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{
+		Token: p.curToken,
+	}
+
+	value, err := strconv.ParseInt(lit.Token.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
