e88c2233393d6059bd29cd4e027ad35b062a3644
Arjun Malhotra <arjun.malhotra@openly.com>
2023-08-30 08:07:16 -0500

    Add FunctionalLiteral struct

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/arjunmalhotra1/monkey-interpreter/token"
 )
@@ -205,6 +206,32 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+type FunctionalLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionalLiteral) expressionNode()      {}
+func (fl *FunctionalLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionalLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
