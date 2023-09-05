92fb8dd97dc77cbf8d71897ff5027243d6e84fb3
Arjun Malhotra <arjun1malhotra@gmail.com>
2023-09-05 08:55:05 -0500

    Fix Parsing Integer literal, with parsing program & statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,9 +7,29 @@ import (
 )
 
 func Eval(node ast.Node) object.Object {
+
+	// "type" is the concrete data type that satisfies the interface. Like, Expression is an interface but
+	// "ast.IntegerLiteral" is the data type that satisfies the interface.
 	switch node := node.(type) {
+
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
 }
