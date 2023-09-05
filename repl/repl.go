d0e84476475b1e6e13686e7ea0271303311b45a0
Arjun Malhotra <am2cj@virginia.edu>
2023-09-05 15:38:01 -0500

    Add evaluation in the repl.

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/arjunmalhotra1/monkey-interpreter/evaluator"
 	"github.com/arjunmalhotra1/monkey-interpreter/lexer"
 	"github.com/arjunmalhotra1/monkey-interpreter/parser"
 )
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
