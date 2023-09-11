53dd4228a7a7ff2255b1f9e1c26a30b31513a3f9
Arjun Malhotra <am2cj@virginia.edu>
2023-09-10 23:28:24 -0500

    Add environment to Eval, tests and Repl.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -40,10 +40,10 @@ func TestEvalIntegerExpression(t *testing.T) {
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
-
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/arjunmalhotra1/monkey-interpreter/evaluator"
 	"github.com/arjunmalhotra1/monkey-interpreter/lexer"
+	"github.com/arjunmalhotra1/monkey-interpreter/object"
 	"github.com/arjunmalhotra1/monkey-interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
@@ -30,7 +32,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
