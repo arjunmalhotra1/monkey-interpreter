17ae9ef84ebe2f0384d3d2d6e93a24f140aeac62
Arjun Malhotra <am2cj@virginia.edu>
2023-09-12 17:19:41 -0500

    Fix a typing error.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -624,7 +624,7 @@ func TestCallExpressionParsing(t *testing.T) {
 	checkParserErrors(t, p)
 
 	if len(program.Statements) != 1 {
-		t.Fatalf("program.Statements deos not contain %d statements. got=%d\n", 1, len(program.Statements))
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n", 1, len(program.Statements))
 	}
 
 	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
