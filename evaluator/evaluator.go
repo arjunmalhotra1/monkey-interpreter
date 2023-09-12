89b13f1621a0b4c9fdb0a8a05fa1b553c3e3b748
Arjun Malhotra <am2cj@virginia.edu>
2023-09-12 17:20:24 -0500

    Add case for callExpression in Eval.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -80,8 +80,17 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 
 		return &object.Function{Parameters: params, Env: env, Body: body}
-	}
 
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+	}
 	return nil
 }
 
@@ -240,4 +249,17 @@ func isError(obj object.Object) bool {
 		return obj.Type() == object.ERROR_OBJ
 	}
 	return false
+}
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
