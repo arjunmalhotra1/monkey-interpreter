aca666580e89f0ccccb5bb702e41716d0b166dfc
Arjun Malhotra <am2cj@virginia.edu>
2023-10-08 17:19:22 -0500

    Add test for evaluating string literal.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -339,3 +339,17 @@ func TestClosures(t *testing.T) {
 			addTwo(3);`
 	testIntegerObject(t, testEval(input), 5)
 }
+
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
